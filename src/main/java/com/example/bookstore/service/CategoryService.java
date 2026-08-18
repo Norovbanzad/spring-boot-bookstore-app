0e3075a72832b370cacbd561d1a94a38dc387295
Nora <nora.munkhsukh@gmail.com>
2026-08-18 16:31:25 +0800

    feat: updated category entity, category repository, services

---
+++ src/main/java/com/example/bookstore/service/CategoryService.java
@@ -0,0 +1,49 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.BookstoreApplication;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.bookstore.entity.Category;
+import com.example.bookstore.repository.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	//private final BookstoreApplication bookstoreApplication;
+	private final CategoryRepository categoryRepository;
+	
+	public CategoryService(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
+	
+	public List<Category> findAllCategories() {
+		return categoryRepository.findAll();
+	};
+	
+	public Category findCategoryById(Long id) {
+		return categoryRepository.findById(id).orElseThrow();
+	}
+	
+	// Create
+	@Transactional
+	public Category create(Category category) {
+		return categoryRepository.save(category);
+	}
+	
+	// Update
+	public Category updateCategory(Long id, Category newCategory) {
+		Category foundCategory = categoryRepository.findById(id).orElseThrow();
+		foundCategory.setName(newCategory.getName());
+		return categoryRepository.save(foundCategory);
+	}
+	
+	// Delete
+	public void deleteCategory(Long id) {
+		Category founCategory = categoryRepository.findById(id).orElseThrow();
+		categoryRepository.delete(founCategory);
+	}
+}
--- src/main/java/com/example/bookstore/web/BookNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookstore.web;
-
-public class BookNotFoundException extends RuntimeException {
-
-    public BookNotFoundException(Long id) {
-        super("Book not found with id: " + id);
-    }
-}
